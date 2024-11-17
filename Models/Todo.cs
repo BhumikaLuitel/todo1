1f6c9378697e7404e9a88c1c53eb19588987e382
Kamana <bhumikaluitel09@gmail.com>
2024-11-17 20:38:16 +0545

    initial

---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string Employee { get; set; }
+        public string Work { get; set; }
+        public string Desription { get; set; }
+        public DateTime Duration   { get; set; }
+    }
+}
